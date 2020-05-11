0846bf4e2358c0a8d5a188669eb55a20ea90bdd6
csdoker <csd758371536@qq.com>
2020-05-11 16:19:32 +0800

    完成Nav组件样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <router-view/>
-    <hr>
   </div>
 </template>
 
@@ -10,6 +9,13 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+body {
+  line-height: 1.5;
+}
+a {
+  text-decoration: none;
+  color: inherit;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,19 @@
+
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="money"></Icon>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels">
-      <Icon name="label"></Icon>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics">
-      <Icon name="statistics"></Icon>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -22,7 +23,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  
-}
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      padding: 2px 0;
+      width: calc(100% / 3);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    > .item.selected{
+      color: red;
+    }
+  }
 </style>
