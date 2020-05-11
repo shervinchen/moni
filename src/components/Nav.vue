d44f6e698ec922fe6bc309c64ff0fcc9f0cf172f
csdoker <csd758371536@qq.com>
2020-05-11 15:58:47 +0800

    引入svg-sprite-loader插件，封装Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"></Icon>
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label"></Icon>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"></Icon>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Nav'
+  name: "Nav",
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid red;
-  }
+.nav {
+  
+}
 </style>
