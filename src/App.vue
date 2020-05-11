0982c9ba36c8340cd836f044180221c69e5f91b2
csdoker <csd758371536@qq.com>
2020-05-11 14:14:14 +0800

    money页面布局

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">记账</router-link>
     <router-link to="/labels">标签</router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+export default {
+  name: 'Nav'
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .nav {
+    border: 1px solid red;
+  }
+</style>
