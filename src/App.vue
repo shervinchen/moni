06275759b0f1a10e729f33f62f543c04eae192ab
csdoker <csd758371536@qq.com>
2020-05-12 09:00:23 +0800

    添加全局样式和变量、完成Money组件基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,18 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/helper.scss";
+@import "~@/assets/style/reset.scss";
+
 body {
+  font-family: $font-hei;
   line-height: 1.5;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #333;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,6 +23,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '~@/assets/style/helper.scss';
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -41,7 +42,7 @@ export default {
       }
     }
     > .item.selected{
-      color: red;
+      color: $color-highlight;
     }
   }
 </style>
