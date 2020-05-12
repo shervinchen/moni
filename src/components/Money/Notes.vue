7fc7cec4a7e2055ac03216fee5b6049c50875b2a
csdoker <csd758371536@qq.com>
2020-05-12 15:34:42 +0800

    使用v-model替代value写法

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,7 +1,7 @@
 <template>
   <label class="notes">
     <span class="name">备注</span>
-    <input type="text" :value="value" @input="onInput" placeholder="在这里输入备注" />
+    <input type="text" v-model="value" placeholder="在这里输入备注" />
   </label>
 </template>
 
@@ -12,11 +12,6 @@ import { Component } from "vue-property-decorator";
 @Component
 export default class Notes extends Vue {
   value = '';
-  
-  onInput(event: KeyboardEvent) {
-    const input = event.target as HTMLInputElement
-    this.value = input.value
-  }
 }
 </script>
 
