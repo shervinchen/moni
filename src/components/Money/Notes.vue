bca98d4c1e3f076a39c3ffa7ad5a5b1b9b42d4fd
csdoker <csd758371536@qq.com>
2020-05-12 10:57:55 +0800

    完成Money.vue的模块化重构，拆分为四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+    <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注" />
+    </label>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Notes'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.notes {
+  font-size: 14px;
+  display: block;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
