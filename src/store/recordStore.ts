68bce21c77ca88683459a38cfb5cdd3ede382c1e
csdoker <csd758371536@qq.com>
2020-05-13 17:43:10 +0800

    将store拆分为recordStore与tagStore

---
+++ src/store/recordStore.ts
@@ -0,0 +1,9 @@
+import recordListModel from "@/models/recordListModel";
+
+export default {
+  // record store
+  recordList: recordListModel.fetch(),
+  createRecord: (record: RecordItem) => {
+    recordListModel.create(record);
+  },
+};
