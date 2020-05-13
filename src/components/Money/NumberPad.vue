7325c9c6476cd0bbd66fa6114944993580de3b7d
csdoker <csd758371536@qq.com>
2020-05-13 08:53:14 +0800

    新增createdAt字段

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -64,6 +64,7 @@ export default class NumberPad extends Vue {
   ok() {
     this.$emit('update:value', parseFloat(this.output));
     this.$emit('submit', this.output)
+    this.output = '0'
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
@@ -12,17 +11,22 @@
 import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
 
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
+
+const recordList: Record[] = JSON.parse(
+    window.localStorage.getItem("recordList") || "[]"
+  );
 
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
-}
+  createdAt?: Date;
+};
 
 @Component({
   components: {
@@ -30,42 +34,43 @@ type Record = {
     Types,
     Notes,
     Tags,
-  }
+  },
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行']
-  recordList: Record[] = []
+  tags = ["衣", "食", "住", "行"];
+  recordList: Record[] = recordList
 
   record: Record = {
     tags: [],
-    notes: '',
-    type: '-',
-    amount: 0
-  }
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
 
   onUpdateTags(value: string[]) {
-    this.record.tags = value
+    this.record.tags = value;
   }
 
   onUpdateNotes(value: string) {
-    this.record.notes = value
+    this.record.notes = value;
   }
 
   saveRecord() {
-    const recordCopy = JSON.parse(JSON.stringify(this.record))
-    this.recordList.push(recordCopy)
+    const recordCopy: Record = JSON.parse(JSON.stringify(this.record));
+    recordCopy.createdAt = new Date();
+    this.recordList.push(recordCopy);
   }
 
-  @Watch('recordList')
+  @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem('recordList', JSON.stringify(this.recordList))
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
   }
 }
 </script>
 
 <style lang="scss">
-  .layout-content {
-    display: flex;
-    flex-direction: column-reverse;
-  }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
 </style>
