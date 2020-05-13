0f61ba39c859cee26ecb7fa1443bd23f8cd8f24b
csdoker <csd758371536@qq.com>
2020-05-13 11:26:58 +0800

    新增EditLabel组件，并添加相关路由

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-        
-    </div>
+  <Layout> </Layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import tagsListModel from "@/models/tagListModel";
 
 @Component
 export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagsListModel.fetch();
+    const tags = tagsListModel.data;
+    const tag = tags.filter((tag) => tag.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
