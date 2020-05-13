743dc7adbfe4f896e9c2a7d5fc083375e2b38845
csdoker <csd758371536@qq.com>
2020-05-13 14:17:01 +0800

    导出Button组件模块

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,6 +2,15 @@
   <button class="button" @click="$emit('click', $event)"><slot></slot></button>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Button extends Vue {
+}
+</script>
+
 <style lang="scss" scoped>
 .button {
   background: #767676;
