d306ecc2ceb90b370ff488a1af995caf3ef092b2
csdoker <csd758371536@qq.com>
2020-05-15 22:09:06 +0800

    添加部署的配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/moni/'
+    : '/',
   lintOnSave: false,
   chainWebpack: (config) => {
     const dir = path.resolve(__dirname, "src/assets/icons");
