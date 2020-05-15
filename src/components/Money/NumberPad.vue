f1f157b71233073c4fb46df302558f474cd0cd12
csdoker <csd758371536@qq.com>
2020-05-15 15:00:38 +0800

    完成统计页面的逻辑

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,7 +26,7 @@ import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class NumberPad extends Vue {
-  @Prop() readonly value!: number;
+  @Prop(Number) readonly value!: number;
   output = this.value.toString()
 
   inputContent(event: MouseEvent) {
@@ -62,8 +62,9 @@ export default class NumberPad extends Vue {
   }
 
   ok() {
-    this.$emit('update:value', parseFloat(this.output));
-    this.$emit('submit', this.output)
+    const number = parseFloat(this.output)
+    this.$emit('update:value', number);
+    this.$emit('submit', number)
     this.output = '0'
   }
 }
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -7,7 +7,10 @@
     />
     <ol>
       <li v-for="(group, index) in groupedList" :key="index">
-        <h3 class="title">{{ formatTitle(group.title) }}</h3>
+        <h3 class="title">
+          {{ formatTitle(group.title) }}
+          <span>￥{{ group.total }}</span>
+        </h3>
         <ol>
           <li class="record" v-for="item in group.items" :key="item.id">
             <span>{{ tagString(item.tags) }}</span>
@@ -27,8 +30,8 @@ import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 
-import dayjs from 'dayjs'
-import clone from '@/lib/clone';
+import dayjs from "dayjs";
+import clone from "@/lib/clone";
 
 @Component({
   components: {
@@ -37,22 +40,22 @@ import clone from '@/lib/clone';
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? '无' : tags.join(',')
+    return tags.length === 0 ? "无" : tags.join(",");
   }
 
   formatTitle(string: string) {
-    const day = dayjs(string)
-    const now = dayjs()
-    if (day.isSame(now, 'day')) {
-      return '今天'
-    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
-      return '昨天'
-    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-      return '前天'
-    } else if (day.isSame(now, 'year')) {
-      return day.format('M月D日')
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, "day")) {
+      return "今天";
+    } else if (day.isSame(now.subtract(1, "day"), "day")) {
+      return "昨天";
+    } else if (day.isSame(now.subtract(2, "day"), "day")) {
+      return "前天";
+    } else if (day.isSame(now, "year")) {
+      return day.format("M月D日");
     } else {
-      return day.format('YYYY年M月D日')
+      return day.format("YYYY年M月D日");
     }
   }
 
@@ -63,19 +66,39 @@ export default class Statistics extends Vue {
   get groupedList() {
     const { recordList } = this;
     if (recordList.length === 0) {
-      return []
+      return [];
     }
-    const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
-    const result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}]
+    const newList = clone(recordList)
+      .filter((record) => record.type === this.type)
+      .sort(
+        (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
+      );
+    type Result = {
+      title: string;
+      total?: number;
+      items: RecordItem[];
+    }[]
+    const result: Result = [
+      {
+        title: dayjs(newList[0].createdAt).format("YYYY-MM-DD"),
+        items: [newList[0]],
+      },
+    ];
     for (let index = 1; index < newList.length; index++) {
-      const current = newList[index]
-      const last = result[result.length - 1]
-      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
-        last.items.push(current)
+      const current = newList[index];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), "day")) {
+        last.items.push(current);
       } else {
-        result.push({title:dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current] })
+        result.push({
+          title: dayjs(current.createdAt).format("YYYY-MM-DD"),
+          items: [current],
+        });
       }
     }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0)
+    })
     return result;
   }
 
@@ -114,7 +137,7 @@ export default class Statistics extends Vue {
   }
   .record {
     @extend %item;
-    background: #fff
+    background: #fff;
   }
   .notes {
     margin-right: auto;
