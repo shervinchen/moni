3b41a202122a7f2e7eac9aced326107df76c5b00
csdoker <csd758371536@qq.com>
2020-05-15 11:14:43 +0800

    引入dayjs日期库，封装日期格式化方法

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -12,8 +12,8 @@
       :value.sync="interval"
     />
     <ol>
-      <li v-for="(group, index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="group in result" :key="group.title">
+        <h3 class="title">{{ formatTitle(group.title) }}</h3>
         <ol>
           <li class="record" v-for="item in group.items" :key="item.id">
             <span>{{ tagString(item.tags) }}</span>
@@ -35,6 +35,8 @@ import Tabs from "@/components/Tabs.vue";
 import intervalList from "@/constants/intervalList";
 import recordTypeList from "@/constants/recordTypeList";
 
+import dayjs from 'dayjs'
+
 @Component({
   components: {
     Tabs,
@@ -43,6 +45,22 @@ import recordTypeList from "@/constants/recordTypeList";
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',')
+  }
+
+  formatTitle(string: string) {
+    const day = dayjs(string)
+    const now = dayjs()
+    if (day.isSame(now, 'day')) {
+      return '今天'
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天'
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天'
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日')
+    } else {
+      return day.format('YYYY年M月D日')
+    }
   }
 
   get recordList() {
