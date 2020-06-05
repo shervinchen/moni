e9ed92d58642339eab9dfa0cf153a33d7c6278af
csdoker <csd758371536@qq.com>
2020-06-06 00:05:18 +0800

    deploy

---
+++ precache-manifest.8a47a4ec2ce4f783ffa51230c6bd58b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444703b853128f8b4b99",
+    "url": "/moni/css/app.6407f5b3.css"
+  },
+  {
+    "revision": "c8c3f8da85580ba93ff07e3cfa069856",
+    "url": "/moni/img/code.c8c3f8da.png"
+  },
+  {
+    "revision": "49eda19cab477d0d71ffdaedba510617",
+    "url": "/moni/index.html"
+  },
+  {
+    "revision": "444703b853128f8b4b99",
+    "url": "/moni/js/app.61b8b04d.js"
+  },
+  {
+    "revision": "4e0eab85c4d430dab00e",
+    "url": "/moni/js/chunk-vendors.3c9bb34e.js"
+  },
+  {
+    "revision": "0941344b2a2e665b49c71310caf613a5",
+    "url": "/moni/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/moni/robots.txt"
+  }
+]);
